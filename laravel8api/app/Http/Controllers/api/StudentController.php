5f91993bd29a6f67eed2d04aac31a60bf35f9bb6
Tyson Bernard <tyson564@hotmail.com>
2023-07-03 18:27:49 -0500

    Funda Of WEB IT - React JS with Laravel 8 API

---
+++ laravel8api/app/Http/Controllers/api/StudentController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $students = Student::get();
+
+        return response()->json([
+            'status' => 200,
+            'students' => $students, 
+            'message' => 'get request executed successfully!'
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $student = Student::create([
+            'name' => $request->input('name'),
+            'course' => $request->input('course'),
+            'email' => $request->input('email'),
+            'phone' => $request->input('phone')
+        ]);
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Student Added Successfully!'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
